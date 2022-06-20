2bc8f72a5b220b964190613a67a87afbf80421ff
임완택 <57139287+beckhem96@users.noreply.github.com>
2022-06-21 07:08:15 +0900

    Update 로또.py

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -14,14 +14,14 @@
 # 위 코드로 풀면 시간초과가 난다.
 
 def dfs(depth, idx):
-    if depth == 6:
+    if depth == 6: # 로또 번호 다 쓰면 출력
         print(*out)
         return
 
-    for i in range(idx, K):
-        out.append(S[i])
-        dfs(depth + 1, i + 1)
-        out.pop()
+    for i in range(idx, K): 
+        out.append(S[i]) 
+        dfs(depth + 1, i + 1) # depth를 하나씩 더 해서 6개의 번호 출력하도록 만듦, i+1은 들어간 수의 인덱스 append 하지 않도록 해줌
+        out.pop() # 길이가 6됐을 때 마지막으로 쓴 수는 뺴줌
 
 while 1:
     input_arr = []
