e9c06160c1934e7be0ae66bab3094bd0726a290a
임완택 <57139287+beckhem96@users.noreply.github.com>
2023-02-02 01:01:36 +0900

    [Gold V] Title: 톱니바퀴, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,38 +1,45 @@
-from collections import deque
+import sys
+# import heapq
+# from collections import deque
+# import copy
+input = sys.stdin.readline
+# sys.setrecursionlimit(10**6)
 
+arr = []
+tasks = []
+result = 0
+for _ in range(4):
+    arr.append(list(map(int, list(input().strip()))))
 
-def rotate_right(x, d):
-    if x > 4 or gears[x - 1][2] == gears[x][6]:
-        return
+k = int(input())
+for _ in range(k):
+    tasks.append(tuple(map(int, input().split())))
 
-    if gears[x - 1][2] != gears[x][6]:
-        rotate_right(x + 1, -d)
-        gears[x].rotate(d)
+for n, w in tasks:
+    q = [(n-1, w)] # 몇 번째 톱니 바퀴인지
+    task = [(n-1, w)] # 돌릴 톱니 바퀴 리스트
+    visited = [0, 0, 0, 0]
+    visited[n-1] = 1
+    while q: # 4개 톱니바퀴에서 맞닿아 있는 부분이 다른 거 찾기
 
+        i, dw = q.pop(0)
+        for di in [1, -1]:
+            ni = i + di
+            if 0<=ni<4 and di == -1 and arr[i][6] != arr[ni][2] and not visited[ni]:
+                task.append((ni, -dw))
+                q.append((ni, -dw))
+                visited[ni] = 1
+            elif 0<=ni<4 and di == 1 and arr[i][2] != arr[ni][6] and not visited[ni]:
+                task.append((ni, -dw))
+                q.append((ni, -dw))
+                visited[ni] = 1
+    for a, b in task:
+        if b == 1:
+            arr[a] = [arr[a][-1]] + arr[a][:-1]
+        else:
+            arr[a] = arr[a][1:] + [arr[a][0]]
 
-def rotate_left(x, d):
-    if x < 1 or gears[x][2] == gears[x + 1][6]:
-        return
-
-    if gears[x][2] != gears[x + 1][6]:
-        rotate_left(x - 1, -d)
-        gears[x].rotate(d)
-
-
-gears = {}
-for i in range(1, 5):
-    gears[i] = deque((map(int, input())))
-
-for _ in range(int(input())):
-    x, d = map(int, input().split())
-
-    rotate_right(x + 1, -d)
-    rotate_left(x - 1, -d)
-    gears[x].rotate(d)
-
-
-ans = 0
 for i in range(4):
-    ans += gears[i + 1][0] * (2 ** i)
-
-print(ans)
+    if arr[i][0] == 1:
+        result += 2**i
+print(result)
