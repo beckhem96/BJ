87607db1beb47c07c2cdccfe8a63d1b062c3de70
임완택 <57139287+beckhem96@users.noreply.github.com>
2023-02-13 00:09:52 +0900

    [Gold III] Title: 줄 세우기, Time: 208 ms, Memory: 38840 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,28 +1,24 @@
 import sys
 from collections import deque
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-
-graph = [[] for _ in range(n+1)]
-inDegree = [0 for _ in range(n+1)]
-queue = deque()
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+degree = [0] * (N+1)
 answer = []
-
-for i in range(m):
-    a, b = map(int, sys.stdin.readline().rstrip().split())
+queue = deque()
+for _ in range(M):
+    a, b = map(int, input().split())
     graph[a].append(b)
-    inDegree[b] += 1
-
-for i in range(1, n+1):
-    if inDegree[i] == 0:
+    degree[b] += 1 # 진입 차수 표시
+for i in range(1, N+1):
+    if degree[i] == 0:
         queue.append(i)
-
 while queue:
     tmp = queue.popleft()
     answer.append(tmp)
     for i in graph[tmp]:
-        inDegree[i] -= 1
-        if inDegree[i] == 0:
+        degree[i] -= 1
+        if degree[i] == 0:
             queue.append(i)
-
 print(*answer)
